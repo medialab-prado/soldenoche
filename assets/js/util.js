e6a9faf85b75552e92f8de818fa498000890c05f
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2019-11-28 18:29:28 +0100

    Se agregaron los videos y el script para autoplay

--- assets/js/util.js
+++ assets/js/util.js
@@ -584,4 +584,55 @@
 
 	};
 
+  /* Video playlist*/
+  var video_player = document.getElementById("video_player");
+  video = video_player.getElementsByTagName("video")[0],
+  video_links = video_player.getElementsByTagName("figcaption")[0],
+  source = video.getElementsByTagName("source"),
+  link_list = [],
+  path = 'videos/',
+  currentVid = 0,
+  allLnks = video_links.children,
+  lnkNum = allLnks.length;
+  video.removeAttribute("controls");
+  video.removeAttribute("poster");
+
+	function playVid(index) {
+		video_links.children[index].classList.add("currentvid");
+		source[0].src = path + link_list[index] + ".mp4";
+		currentVid = index;
+		video.load();
+		video.play();
+	}
+	for (var i=0; i<lnkNum; i++) {
+		var filename = allLnks[i].href;
+		link_list[i] = filename.match(/([^\/]+)(?=\.\w+$)/)[0];
+			(function(index){
+				allLnks[i].onclick = function(i){
+					i.preventDefault();
+						for (var i=0; i<lnkNum; i++) {
+							allLnks[i].classList.remove("currentvid");
+						}
+					playVid(index);
+				}
+		})(i);
+  }
+  video.addEventListener('ended', function () {
+	allLnks[currentVid].classList.remove("currentvid");
+	if ((currentVid + 1) >= lnkNum) {
+		nextVid = 0;
+	} else {
+		nextVid = currentVid+1;
+	}
+	playVid(nextVid);
+  })
+  video.addEventListener('mouseenter',
+	function() {
+		video.setAttribute("controls","true");
+	})
+	video.addEventListener('mouseleave', function() {
+		video.removeAttribute("controls");
+	})
+
+
 })(jQuery);
